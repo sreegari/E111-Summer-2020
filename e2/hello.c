8a39f052193e3e22ad9b872e09e6d911dc72fe25
Sree Garimella <sreegarimella@Srees-MacBook-Pro.local>
2020-06-23 22:14:27 -0500

    hello.c added

--- e2/hello.c
+++ e2/hello.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+#include <unistd.h>
+
+int main()
+{
+    int a = 1;
+    while ( a <= 20 )
+    {
+        printf ( "Hello World\n" );
+        a ++;
+        sleep(3);
+    }
+    return 0;
+}
